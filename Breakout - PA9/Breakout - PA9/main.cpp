15e41ad543ab04b74f4d4b5f31781756b94e483e
Justin Prachar <justin.prachar@yahoo.com>
2020-04-28 15:39:00 -0700

    Added a few files; Some drawing in main

--- Breakout - PA9/Breakout - PA9/main.cpp
+++ Breakout - PA9/Breakout - PA9/main.cpp
@@ -1,13 +1,44 @@
-#include <SFML/Graphics.hpp>
+#include "libs.h"
 
 int main(void)
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	//create window
+	sf::RenderWindow window(sf::VideoMode(700, 700), "Breakout!");
+	window.setFramerateLimit(60);
+
+	//creat font
+	sf::Font font;
+	font.loadFromFile("Roboto-Black.ttf");
+
+
+	//create ball shape
+	sf::CircleShape ball(7.f);
+	ball.setFillColor(sf::Color::Green);
+	ball.setPosition(350, 640);
+
+	//create paddle shape
+	sf::RectangleShape player(sf::Vector2f(70, 10));
+	player.setFillColor(sf::Color::White);
+	player.setPosition(322, 656);
+
+
+
+
+	//sf::CircleShape shape(100.f);
+	//shape.setFillColor(sf::Color::Green);
 
 	while (window.isOpen())
 	{
+		sf::Vector2f ballPos = ball.getPosition();
+		sf::Vector2f playerPos = player.getPosition();
+
+		//detect collisions
+
+		//move ball
+
+		//input detection
+
+			//close window
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
@@ -15,9 +46,16 @@ int main(void)
 				window.close();
 		}
 
+		//render
 		window.clear();
-		window.draw(shape);
+		window.draw(ball);
+		window.draw(player);
 		window.display();
+
+
+
 	}
+//	sf::RenderWindow window(sf::VideoMode(600, 972), "Breakout");
+
 	return 0;
 }
