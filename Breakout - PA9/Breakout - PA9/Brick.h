5245415dabd6b6896e538b94fd2d6780afa100ff
RobertFrawley <63368801+RobertFrawley@users.noreply.github.com>
2020-04-29 13:54:38 -0700

    Update Brick.h
    Brick definition

--- Breakout - PA9/Breakout - PA9/Brick.h
+++ Breakout - PA9/Breakout - PA9/Brick.h
@@ -1,1 +1,14 @@
 #pragma once
+struct Brick
+{
+	sf::RectangleShape shape;
+	bool destroyed{ false };
+	Brick(float x, float y)
+	{
+		shape.setPosition(x, y);
+		shape.setSize(sf::Vector2f(100.f, 25.f));
+		shape.setFillColor(sf::Color::Blue);
+		shape.setOrigin(sf::Vector2f(100 / 2, 50 / 2));
+	}
+
+};
