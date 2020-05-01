e66ff5b2c5cfbf5ba771c30f19c3be8a3cf546d6
RobertFrawley <63368801+RobertFrawley@users.noreply.github.com>
2020-05-01 10:22:25 -0700

    Update Brick.h
    Adds specific float values for specific ways the ball makes collisions

--- Breakout - PA9/Breakout - PA9/Brick.h
+++ Breakout - PA9/Breakout - PA9/Brick.h
@@ -11,4 +11,10 @@ struct Brick
 		shape.setOrigin(sf::Vector2f(100 / 2, 50 / 2));
 	}
 
+	float x() { return shape.getPosition().x; };
+	float y() { return shape.getPosition().y; };
+	float left() { return x() - shape.getSize().x / 2.f; };
+	float right() { return x() + shape.getSize().x / 2.f; };
+	float top() { return y() - shape.getSize().y / 2.f; };
+	float bottom() { return y() + shape.getSize().y / 2.f; };
 };
