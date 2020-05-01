e610ee58286e76cbeb630ef4ea529dcd406c9b2f
RobertFrawley <63368801+RobertFrawley@users.noreply.github.com>
2020-05-01 10:23:03 -0700

    Update Collisions.h
    Collision Detection updated

--- Breakout - PA9/Breakout - PA9/Collisions.h
+++ Breakout - PA9/Breakout - PA9/Collisions.h
@@ -1,1 +1,31 @@
-#pragma once
+template <class T1, class T2> bool Intersecting(T1& A, T2& B)
+{
+	return A.right() >= B.left() && A.left() <= B.right() && A.bottom() >= B.top() && A.top() <= B.bottom();
+}
+
+void Collision(Brick& Brick,  Ball& Ball)
+{
+	Brick.destroyed = true;
+
+	float overlapleft { Ball.right() - Brick.left() };
+	float overlapright{ Brick.right() - Ball.left() };
+	float overlaptop{ Ball.bottom() - Brick.top() };
+	float overlapbottom{ Brick.bottom() - Ball.top() };
+
+	bool BallLeft(abs(overlapleft) < abs(overlapright));
+
+	bool BallTop(abs(overlaptop) < abs(overlapbottom));
+
+	float minoverlapX{ BallLeft ? overlapleft : overlapright };
+
+	float minoverlapY{ BallTop ? overlaptop : overlapbottom };
+
+	if (abs(minoverlapX) < abs(minoverlapY))
+	{
+		Ball.velocity.x = BallLeft ? -5 : 5;
+	}
+	else
+	{
+		Ball.velocity.y = BallTop ? -5 : 5;
+	}
+}
