da9fb854717c23c0cbce4708d7b8f314b3c9ba70
Justin Prachar <justin.prachar@yahoo.com>
2020-05-01 22:00:32 -0700

    Gave brick real inheritence

--- Breakout - PA9/Breakout - PA9/Brick.h
+++ Breakout - PA9/Breakout - PA9/Brick.h
@@ -1,36 +1,12 @@
-class Brick
+class RectangleBase
 {
 public:
 	sf::RectangleShape shape;
-	bool destroyed{ false };
-	int Armor = 0;
-	Brick(float x, float y)
-	{
-		int check = rand() % 3 + 1;
 
-		if (check == 1)
-		{
-			shape.setPosition(x, y);
-			shape.setSize(sf::Vector2f(100.f, 25.f));
-			shape.setFillColor(sf::Color::Blue);
-			shape.setOrigin(sf::Vector2f(100 / 2, 50 / 2));
-		}
-		else if (check == 2)
-		{
-			shape.setPosition(x, y);
-			shape.setSize(sf::Vector2f(100.f, 25.f));
-			shape.setFillColor(sf::Color::Green);
-			shape.setOrigin(sf::Vector2f(100 / 2, 50 / 2));
-			Armor = 1;
-		}
-		else
-		{
-			shape.setPosition(x, y);
-			shape.setSize(sf::Vector2f(100.f, 25.f));
-			shape.setFillColor(sf::Color::Red);
-			shape.setOrigin(sf::Vector2f(100 / 2, 50 / 2));
-			Armor = 2;
-		}
+	RectangleBase(float x, float y, float width, float height)
+	{
+		shape.setPosition(x, y);
+		shape.setSize(sf::Vector2f(width, height));
 	}
 
 	float x() { return shape.getPosition().x; };
@@ -41,17 +17,44 @@ public:
 	float bottom() { return y() + shape.getSize().y / 2.f; };
 };
 
-class Paddle : public Brick
+class Brick : public RectangleBase
 {
 public:
-	sf::RectangleShape shape;
+	bool destroyed{ false };
+	int Armor = 0;
+	Brick(float x, float y):RectangleBase(x,y,100.f,25.f)
+	{
+		int check = rand() % 3 + 1;
+
+		if (check == 1)
+		{
+			shape.setFillColor(sf::Color::Blue);
+			shape.setOrigin(sf::Vector2f(100 / 2, 50 / 2));
+		}
+		else if (check == 2)
+		{
+			shape.setFillColor(sf::Color::Green);
+			shape.setOrigin(sf::Vector2f(100 / 2, 50 / 2));
+			Armor = 1;
+		}
+		else
+		{
+			shape.setFillColor(sf::Color::Red);
+			shape.setOrigin(sf::Vector2f(100 / 2, 50 / 2));
+			Armor = 2;
+		}
+	}
+
+};
+
+class Paddle : public RectangleBase
+{
+public:
 	sf::Vector2f velocity{ 0, 0 };
 
-	Paddle(float x, float y) : Brick(x, y)
+	Paddle(float x, float y):RectangleBase(x,y,70,10)
 	{
-		shape.setSize(sf::Vector2f(70, 10));
 		shape.setFillColor(sf::Color::White);
-		shape.setPosition(x, y);
 		shape.setOrigin(70 / 2, 10 / 2);
 	}
 
@@ -68,11 +71,4 @@ public:
 			velocity.x = 0;
 
 	};
-
-	float x() { return shape.getPosition().x; };
-	float y() { return shape.getPosition().y; };
-	float left() { return x() - shape.getSize().x / 2.f; };
-	float right() { return x() + shape.getSize().x / 2.f; };
-	float top() { return y() - shape.getSize().y / 2.f; };
-	float bottom() { return y() + shape.getSize().y / 2.f; };
 };
